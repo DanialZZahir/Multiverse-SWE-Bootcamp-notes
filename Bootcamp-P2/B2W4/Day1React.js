425ae4d91a5c169b699cc1e3c54c17af2c8471fb
Danialzzahir <danialzzahir@gmail.com>
2022-08-25 03:20:26 +0100

    added exmaples

--- Bootcamp-P2/B2W4/Day1React.js
+++ Bootcamp-P2/B2W4/Day1React.js
@@ -1,4 +1,16 @@
 // & ---------------- The DOM ----------------
 
 // ? Javascript uses the Document Object Model to manipulate HTML elements on webpages
+// ? DOM is an API used in Javacript for HTML
 
+// ^ What you can do with the DOM:
+// ^    1) Create, Read, Update and Delete HTML elements
+// ^    2) Navigate the structure and style of HTML elements
+// ^    3) Reference HTML documents
+
+// & the 'document.' keyword refers to all the contents within the HTML file 
+// & which is linked to the javascript file in which the 'document.' keyword is used
+
+// & examples:
+// &    document.getElementById("description" (if you gave an Id="description" to any of your HTML elements)
+// &    document.getElementByTagName("Name of element" (I.e. h1, h2, p etc.))
